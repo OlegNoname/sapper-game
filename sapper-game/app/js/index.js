fea62670448d63baafc3ed44b1580646d7138c4b
OlegParfenov <55343121+OlegParfenov@users.noreply.github.com>
2019-12-14 18:27:16 +0300

    v0.4.1 - Написан скрипт добавления/снятия флага с клетки

--- sapper-game/app/js/index.js
+++ sapper-game/app/js/index.js
@@ -1,5 +1,23 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
-        // alert('check');
+        const
+            cells = document.querySelector('.cells');
+        //Обработчик нажатия ПКМ на любую клетку внутри игрового поля
+        cells.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            let target = event.target;
+            if (target.classList.contains('cell')) {
+
+                setClearFlag(target);
+            }
+        })
+        //Функция добавления/снятия флага
+        function setClearFlag(cell) {
+            if (cell.classList.contains('cell_flag')) {
+                cell.classList.remove('cell_flag');
+            }
+            else cell.classList.add('cell_flag');
+
+        }
     }, false);
 }());
